04e96328696239d09b5dd882129833b8ec6fe933
izzy <50057075+hitokizzy@users.noreply.github.com>
2023-02-15 00:06:56 +0700

    update

--- Sriasih/__init__.py
+++ Sriasih/__init__.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 if path.exists("config.env"):
     load_dotenv("config.env")
     
-    
+BANNED_USER = environ.get("BANNED_USER" or None)  
 BOT_TOKEN = environ.get("BOT_TOKEN", None)
 API_ID = int(environ.get("API_ID", 6))
 API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
