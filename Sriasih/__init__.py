67a4ab77dd5882a9cf71268f384ae72881d243c3
izzy <50057075+hitokizzy@users.noreply.github.com>
2023-02-14 23:40:59 +0700

    update

--- Sriasih/__init__.py
+++ Sriasih/__init__.py
@@ -10,8 +10,31 @@ from pyrogram.types import Message
 from pyromod import listen
 from Python_ARQ import ARQ
 from telegraph import Telegraph
+from os import environ, path
 
-from config import *
+from dotenv import load_dotenv
+
+if path.exists("config.env"):
+    load_dotenv("config.env")
+    
+    
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
+API_ID = int(environ.get("API_ID", 6))
+API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
+MONGO_URL = environ.get("MONGO_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+RSS_DELAY = int(environ.get("RSS_DELAY", None))
+STRING_SESSION = environ.get("STRING_SESSION")
+BLACKLIST_CHAT = environ.get("BLACKLIST_CHAT")
+UPSTREAM_REPO = environ.get(
+    "UPSTREAM_REPO", ""
+)
 
 
 ALIVE_LOGO = "https://telegra.ph/file/5d71546548f5a2e3a896e.png"
